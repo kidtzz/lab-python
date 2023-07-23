e4d7f7d5d84593f1c5fab89510a3318d9fe89015
kidtz <bangkitpr6@gmail.com>
2023-07-23 20:04:57 +0700

    update-lagi bg

--- get-jso-csv.py
+++ get-jso-csv.py
@@ -1,22 +1,55 @@
 import requests
-import csv
 from requests.api import head
+import csv
 import json
 
-apiDetailRecord = 'http://localhost:8000/api/v1/berita/getBerita/'
-apiCountRecord = 'http://localhost:8000/api/v1/berita/getBerita/'
+#list endpoint
+# https://uem.bukitmakmur.com:8383/api/1.4/inventory/compdetailssummary?resid=43260
+
+
+#untuk login get token
+apiLogin = ''  #Masukin Endpoint login disini cuy
+headLogin = {
+  "Accept" : "application/json",
+  "Content-type" : "application/json",
+}
+
+params = {
+  "username": "Input Username disini ----------",
+  "password": "Input password disini -----------"
+}
+
+resp = requests.post(apiLogin, headers = headLogin ,data=json.dumps(params))
+tokennya = json.loads(resp.text)['token']
+
+if resp.status_code != 200:
+  print('error: ' + str(resp.status_code))
+else:
+  print('token: ' + str(tokennya))
+  print('Success')
+ 
+
+#=============================================================================
+#untuk get Data
+apiDetailRecord = 'http://localhost:8000/api/v1/berita/getBerita/' #detail record
+apiCountRecord = 'http://localhost:8000/api/v1/berita/getBerita/' #check jlmh record
 
 headers = {
   "Accept" : "application/json",
-  "Content-type" : "application/json"
+  "Content-type" : "application/json",
+  "Authorization": str(tokennya) #dapetin token sik diatas
 }
 
+
+#=============================================================================
 #Count Record data 
 response = requests.request("GET", apiCountRecord, headers=headers, data={})
 data_json  = response.json()
 total_data = len(data_json['data'])
 print(total_data)
 
+
+#=============================================================================
 #convert to CSV file
 with open ('berita.csv','w', encoding='UTF8', newline='') as f:
   csvheader = ['id','judul','kategori','deskripsi','gambar','user']
@@ -24,7 +57,7 @@ with open ('berita.csv','w', encoding='UTF8', newline='') as f:
   writer.writerow(csvheader)
   # ================
   idplus = 1
-  jlmhrecord = total_data #isi-jumlah datanya dengan API Count
+  jlmhrecord = total_data #Jumlah datanya 
   for idplus in range(jlmhrecord):
     idplus += 1
     valueId = {'id': idplus}
@@ -40,12 +73,12 @@ with open ('berita.csv','w', encoding='UTF8', newline='') as f:
     writer.writerows(ourdata)
   print("Done CSV bg")
 
-
+#=============================================================================
 #Convert to JSON file
 with open('data.json', 'w') as outfile:
   idplus = 0
   cok = []
-  jlmhrecord = total_data #isi-jumlah datanya dengan API Count
+  jlmhrecord = total_data  #jumlah datanya 
   for idplus in range(jlmhrecord):
     idplus += 1
     valueId = {'id': idplus}
