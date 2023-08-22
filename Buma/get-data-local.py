adf85f37f5e9aed54b81f05e1b573ac77ae726ba
kidtz <bangkitpr4@gmail.com>
2023-08-22 20:14:47 +0700

    update

--- Buma/get-data-local.py
+++ Buma/get-data-local.py
@@ -6,9 +6,9 @@ import json
 
 
 #=============contoh yg benernya=======
-f = open('buma_error_response.json')
+f = open('installed_software.json')
 data = json.load(f)
-print("status apa?", data[0]['status'])
+# print("status apa?", data[0]['status'])
 
 with open ('test.csv','w', encoding='UTF8', newline='') as f:
   csvheader = ['tes2222','tes2','tes3']
@@ -22,12 +22,22 @@ with open ('test.csv','w', encoding='UTF8', newline='') as f:
     ourdata = []
     for x in range(len(data)):
       if data[x]['status'] == "success":
-        for i in data:
-          listing = [i['message_response']['compdetailssummary']['asset_summary']['commercial_software'],i['message_response']['compdetailssummary']['computer_os_summary']['os_name'],i['message_response']['compdetailssummary']['computer_summary']['computer_name']]
-        ourdata.append(listing)
-        writer.writerows(ourdata)
+        # print(data[x]['message_response']['installedsoftware'])
+        for i in data[x]['message_response']['installedsoftware']:
+          listing = [i['user_name'],i['software_name']]
+          writer.writerows([listing])
+        # for i in data:
+        #   listing = [i['message_response']['compdetailssummary']['asset_summary']['commercial_software'],i['message_response']['compdetailssummary']['computer_os_summary']['os_name'],i['message_response']['compdetailssummary']['computer_summary']['computer_name']]
+        # ourdata.append(listing)
+        # writer.writerows(ourdata)
       elif data[x]['status'] == "error":
         print("skip id ke - ", idplus)
 print("Done CSV")
 
 
+#  for idplus in range(jlmhrecord):
+#     ourdata = []
+#     for i in data['message_response']['installedsoftware']:
+#       listing = [i['user_name'],i['software_name']]
+#       writer.writerows([listing])
+#     break
